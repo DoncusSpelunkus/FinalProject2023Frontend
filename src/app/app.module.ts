468edfd2cf5c1c157dd935eb4de0325b100bdaa3
DoncusSpelunkus <marcuspetersen@live.dk>
2023-10-19 11:05:24 +0200

    Display commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './loginComponents/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopnavComponent } from './dashboard/topnav/topnav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
