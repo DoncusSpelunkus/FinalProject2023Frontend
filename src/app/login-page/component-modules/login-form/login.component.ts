4527a5b8969baf5032f2f79a1dc521c0ed2bcd51
Renaldinho <rena0968@easv365.dk>
2023-10-28 15:59:30 +0200

    clean up

--- src/app/login-page/component-modules/login-form/login.component.ts
+++ src/app/login-page/component-modules/login-form/login.component.ts
@@ -4,8 +4,7 @@ import {FormControlNames} from "../../../../constants/input-field-constants";
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
