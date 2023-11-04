d26c2da1fafdc3c54ae026a6abd38d29cb3e1e11
Renaldinho <rena0968@easv365.dk>
2023-11-04 22:35:24 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login-page/component-modules/login-form/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { TopnavComponent } from './dashboard/topnav/topnav.component';
 import {MatSnackBar} from "@angular/material/snack-bar"
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
@@ -29,7 +28,6 @@ import { ShipmentPageComponent } from './shipment-page/shipment-page.component';
         AppComponent,
         LoginComponent,
         DashboardComponent,
-        TopnavComponent,
         LoginPageComponent,
         CollapsableContainerDirective,
         ManageUsersPageComponent,
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,29 @@
-import {Component, HostBinding, signal} from '@angular/core';
+import {AfterViewInit, Component, HostBinding, OnInit, signal, TemplateRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {
-  @HostBinding('style.width') width = '100%';
+export class DashboardComponent implements AfterViewInit{
+
+  currentActionsTemplate: TemplateRef<any>;
+
+  @ViewChild('publicActions') publicActionsTemplate: TemplateRef<any>;
+  @ViewChild('superAdminActions') superAdminActionsTemplate: TemplateRef<any>;
+  @ViewChild('adminActions') adminActionsTemplate: TemplateRef<any>;
+  @ViewChild('userActions') userActionsTemplate: TemplateRef<any>;
 
   isExpanded = false;
+
+  userRole: any;
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    this.userRole = this.getUserRole();
+    this.enableUserActions();
+  }
 
   extendNavigation() {
     this.isExpanded = true;
@@ -17,4 +32,38 @@ export class DashboardComponent {
   collapseNavigation() {
     this.isExpanded = false;
   }
+
+  @HostBinding('style.width') width = '100%';
+
+  private getUserRole() {
+    return ActionTemplates.Admin;
+  }
+
+  private enableUserActions() {
+    const template = this.userRole;
+    switch (template) {
+      case ActionTemplates.Public:
+        this.currentActionsTemplate = this.publicActionsTemplate;
+        break;
+      case ActionTemplates.SuperAdmin:
+        this.currentActionsTemplate = this.superAdminActionsTemplate;
+        break;
+      case ActionTemplates.Admin:
+        this.currentActionsTemplate = this.adminActionsTemplate;
+        break;
+      case ActionTemplates.User:
+        this.currentActionsTemplate = this.userActionsTemplate;
+        break;
+      default:
+        this.currentActionsTemplate = this.publicActionsTemplate;
+    }
+  }
 }
+
+export enum ActionTemplates {
+  Public = 'public',
+  SuperAdmin = 'superAdmin',
+  Admin = 'admin',
+  User = 'user',
+}
+
