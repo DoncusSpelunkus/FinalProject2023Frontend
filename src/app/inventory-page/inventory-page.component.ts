b68325c557be9fc1041ab4410c0f1efa4756760c
Renaldinho <rena0968@easv365.dk>
2023-11-10 19:46:44 +0100

    .

--- src/app/inventory-page/inventory-page.component.ts
+++ src/app/inventory-page/inventory-page.component.ts
@@ -1,18 +1,30 @@
-import {Component, HostBinding} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {DynamicDialogComponent} from "../util/dynamic-dialog/dynamic-dialog.component";
 import {CreateUserComponent} from "../manage-users/create-user/create-user.component";
 import {StockProductComponent} from "./stock-product/stock-product/stock-product.component";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormControlNames} from "../../constants/input-field-constants";
 
 @Component({
   selector: 'app-inventory-page',
   templateUrl: './inventory-page.component.html'
 })
-export class InventoryPageComponent {
+export class InventoryPageComponent implements OnInit{
   @HostBinding('style.width') width = '100%'
   @HostBinding('style.height') height = '100%'
 
-  constructor(private dialog: MatDialog) {
+  FormControlNames = FormControlNames;
+
+  formGroup: FormGroup;
+
+  constructor(
+    private dialog: MatDialog,
+    private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.initializeFormGroup();
   }
 
   openStockProductsWindow() {
@@ -24,5 +36,19 @@ export class InventoryPageComponent {
         inputs: {} // No dependent data to pass
       }
     });
+  }
+
+  clearFilterValue() {
+
+  }
+
+  get filterValue() {
+    return this.formGroup.get(FormControlNames.FILTER)?.value;
+  }
+
+  private initializeFormGroup() {
+    this.formGroup = this.formBuilder.group({
+      [FormControlNames.FILTER]: ['']
+    })
   }
 }
