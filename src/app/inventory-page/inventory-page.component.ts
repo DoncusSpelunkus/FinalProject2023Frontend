8bc0c2cdb723e64ad137df8961382a51f5580652
Renaldinho <rena0968@easv365.dk>
2023-11-11 17:24:15 +0100

    .

--- src/app/inventory-page/inventory-page.component.ts
+++ src/app/inventory-page/inventory-page.component.ts
@@ -1,23 +1,28 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
+import {AfterViewInit, Component, HostBinding, OnInit, ViewChild} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {DynamicDialogComponent} from "../util/dynamic-dialog/dynamic-dialog.component";
-import {CreateUserComponent} from "../manage-users/create-user/create-user.component";
 import {StockProductComponent} from "./stock-product/stock-product/stock-product.component";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {FormControlNames} from "../../constants/input-field-constants";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {ProductLocationDisplayed} from "../../entities/ProductLocation";
 
 @Component({
   selector: 'app-inventory-page',
   templateUrl: './inventory-page.component.html'
 })
-export class InventoryPageComponent implements OnInit{
+export class InventoryPageComponent implements OnInit, AfterViewInit{
   @HostBinding('style.width') width = '100%'
   @HostBinding('style.height') height = '100%'
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   FormControlNames = FormControlNames;
   displayedColumns = ['Product name','Location','Relocate','Adjust quantity'];
 
   formGroup: FormGroup;
+  dataSource = new MatTableDataSource<any>();
 
   constructor(
     private dialog: MatDialog,
@@ -26,6 +31,10 @@ export class InventoryPageComponent implements OnInit{
 
   ngOnInit(): void {
     this.initializeFormGroup();
+  }
+
+  ngAfterViewInit(): void {
+    this.initializeDataSource();
   }
 
   openStockProductsWindow() {
@@ -52,4 +61,45 @@ export class InventoryPageComponent implements OnInit{
       [FormControlNames.FILTER]: ['']
     })
   }
+
+  /**
+   * TODO subscribe to service data value changes and link them
+   * Initialize data source data
+   * @private
+   */
+  private initializeDataSource() {
+    this.dataSource.data = productLocations;
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+const productLocations: ProductLocationDisplayed[] = [
+  {
+    ProductLocationId: 1,
+    ProductSKU: "SKU001",
+    LocationId: 101,
+    Quantity: 50,
+    LastUpdated: new Date("2023-01-01"),
+    WarehouseId: 201,
+    ProductName: "Product A"
+  },
+  {
+    ProductLocationId: 2,
+    ProductSKU: "SKU002",
+    LocationId: 102,
+    Quantity: 60,
+    LastUpdated: new Date("2023-02-01"),
+    WarehouseId: 202,
+    ProductName: "Product B"
+  },
+  // ... add more objects here ...
+  {
+    ProductLocationId: 10,
+    ProductSKU: "SKU010",
+    LocationId: 110,
+    Quantity: 30,
+    LastUpdated: new Date("2023-10-01"),
+    WarehouseId: 210,
+    ProductName: "Product J"
+  }
+];
