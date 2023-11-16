8d4a213760021d1afb33a2937a7c3bfad1159151
DoncusSpelunkus <marcuspetersen@live.dk>
2023-11-16 18:52:01 +0100

    Revert "Revert "fix""
    This reverts commit 7379cd9a4cbe1803b9fa244a05bdb5a2d0070273.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,15 @@ import { AdminGuardService } from 'src/services/AuthGuardSevices/adminGuard.serv
 import { SalesGuardService } from 'src/services/AuthGuardSevices/salesUserGuard.service';
 import {ManageUsersPageComponent} from "./manage-users/manage-users-page/manage-users-page.component";
 import {LogsPageComponent} from "./logs-page/logs-page.component";
-import {WarehouseManagementPageComponent} from "./warehouse-management-page/warehouse-management-page.component";
 import {ShipmentPageComponent} from "./shipment-page/shipment-page.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {InventoryPageComponent} from "./inventory-page/inventory-page.component";
 
 const routes: Routes = [
   { path: 'login', component:  LoginPageComponent},
   { path: "userManagement", component: ManageUsersPageComponent, canActivate: [AdminGuardService]},
   { path: "logs", component: LogsPageComponent, canActivate: [AdminGuardService] },
-  { path: "warehouse", component: WarehouseManagementPageComponent},
+  { path: "inventory", component: InventoryPageComponent},
   { path: "shipment", component: ShipmentPageComponent, canActivate: [SalesGuardService] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '404', component: PageNotFoundComponent },
