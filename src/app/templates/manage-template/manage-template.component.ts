d61107f8a5f3901ec52d2780fa0471923ef4e581
Renaldinho <rena0968@easv365.dk>
2023-12-12 17:48:03 +0100

    .

--- src/app/templates/manage-template/manage-template.component.ts
+++ src/app/templates/manage-template/manage-template.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {FormControlNames} from "../../../constants/input-field-constants";
 import {FormBuilding} from "../../../interfaces/component-interfaces";
+import {getFormControl} from "../../../util/form-control-validators";
 
 @Component({
   selector: 'app-manage-template',
   templateUrl: './manage-template.component.html'
 })
-export class ManageTemplateComponent implements FormBuilding{
+export class ManageTemplateComponent extends FormBuilding{
 
   tableFormGroup: FormGroup;
-
   constructor(private formBuilder: FormBuilder) {
+    super();
     this.initializeFormGroup();
   }
 
@@ -21,5 +22,13 @@ export class ManageTemplateComponent implements FormBuilding{
       [FormControlNames.PAGE]: [''],
       [FormControlNames.ITEMS_PER_PAGE]: ['']
     })
+  }
+
+  get filterValue(): string {
+    return getFormControl(FormControlNames.FILTER,this.tableFormGroup).value;
+  }
+
+  clearFilterValue() {
+    getFormControl(FormControlNames.FILTER,this.tableFormGroup).reset();
   }
 }
