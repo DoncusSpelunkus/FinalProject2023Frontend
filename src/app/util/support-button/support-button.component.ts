d8a31264a2efa14867cbf5478dee1bedf4424f05
Renaldinho <rena0968@easv365.dk>
2023-12-23 02:03:40 +0100

    support button component

--- src/app/util/support-button/support-button.component.ts
+++ src/app/util/support-button/support-button.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormControlNames} from "../../../constants/input-field-constants";
+import {FormBuilding} from "../../../interfaces/component-interfaces";
+import {emailValidator, valueRequired} from "../../../util/form-control-validators";
 
 @Component({
   selector: 'app-support-button',
   templateUrl: './support-button.component.html'
 })
-export class SupportButtonComponent {
+export class SupportButtonComponent extends FormBuilding{
   isOpened = false;
+  isSending = false;
 
-  handleOpenWriteToSupportSection() {
+  formGroup: FormGroup
 
+  constructor(private elRef: ElementRef,
+              private formBuiler: FormBuilder) {
+    super();
+    this.initializeFormGroup();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    if (!this.elRef.nativeElement.contains(event.target)) {
+      this.isOpened = false;
+    }
+  }
+
+  private initializeFormGroup() {
+    this.formGroup = this.formBuiler.group({
+      [FormControlNames.EMAIL]: ['',[emailValidator,valueRequired(FormControlNames.EMAIL)]],
+      [FormControlNames.DESCRIPTION]: ['',[valueRequired(FormControlNames.DESCRIPTION)]]
+    })
+  }
+
+  handleSendEmail() {
+    if (!this.formGroup.valid) {
+      return;
+    }
+    this.isSending = true;
+    this.formGroup.reset();
+    setTimeout(() => {
+      this.isOpened = false;
+      this.isSending = false;
+    },3000);
   }
 }
